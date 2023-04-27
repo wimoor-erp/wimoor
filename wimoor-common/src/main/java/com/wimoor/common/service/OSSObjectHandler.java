acc83648c6cfd3c9eabdb09cb82b493a98403e01
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-04-27 17:42:00 +0800

    深圳市万墨信息科技有限公司
    清空旧文件

--- wimoor-common/src/main/java/com/wimoor/common/service/OSSObjectHandler.java
+++
@@ -1,9 +0,0 @@
-package com.wimoor.common.service;
-
-import java.io.BufferedReader;
-import java.util.Map;
-
-
-public interface OSSObjectHandler {
-	public void treatReader(BufferedReader reader, Map<String, Object> param);
-}
