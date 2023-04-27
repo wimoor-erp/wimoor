a8130e211db24e94b5123d25754f0da71a22e2d3
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-04-27 17:41:23 +0800

    深圳市万墨信息科技有限公司
    清空旧文件

--- wimoor-erp/src/main/java/com/wimoor/erp/inventory/service/IInventoryRecordService.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.erp.inventory.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wimoor.erp.inventory.pojo.entity.InventoryRecord;
-
-public interface IInventoryRecordService extends IService<InventoryRecord>{
-		
-}
