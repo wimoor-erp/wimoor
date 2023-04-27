9222b8a461f1dd6442c30782206fd88102f5561e
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-04-27 17:42:56 +0800

    清空旧文件

--- wimoor-amazon-adv/src/main/java/com/wimoor/amazon/adv/task/service/ISysTimeTaskLogService.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.amazon.adv.task.service;
-
-import com.wimoor.amazon.adv.common.service.IService;
-import com.wimoor.amazon.adv.task.pojo.SysTimeTaskLog;
-
-public interface ISysTimeTaskLogService extends IService<SysTimeTaskLog>{
-
-}
