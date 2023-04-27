6c504ab0d81f298960cb9992fe3e740d1cb8a8e0
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-04-27 17:41:23 +0800

    深圳市万墨信息科技有限公司
    清空旧文件

--- wimoor-erp/src/main/java/com/wimoor/erp/purchase/service/IPurchasePlanModelService.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.erp.purchase.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wimoor.erp.purchase.pojo.entity.PurchasePlanModel;
-
-public interface IPurchasePlanModelService extends IService<PurchasePlanModel> {
-
-}
