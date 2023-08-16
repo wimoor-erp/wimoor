7f5344f9fe285f9dd1fa98b848d0b259a8ed267e
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-08-16 12:13:46 +0800

    重写上传新版本-admin

--- wimoor-admin/admin-boot/src/main/java/com/wimoor/admin/service/ISysOauthClientService.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.admin.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wimoor.admin.pojo.entity.SysOauthClient;
-
-public interface ISysOauthClientService extends IService<SysOauthClient> {
-    void cleanCache();
-}
