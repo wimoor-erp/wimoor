7b9a38052b04111cfeef3e8a9bcda5fa6f9cecd5
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-08-16 12:15:32 +0800

    移除

--- wimoor-erp/erp-boot/src/main/java/com/wimoor/erp/purchase/alibaba/service/IOpen1688MessageService.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.erp.purchase.alibaba.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.wimoor.erp.purchase.alibaba.pojo.entity.Open1688Message;
-
-public interface IOpen1688MessageService extends IService<Open1688Message>{
-
-}
