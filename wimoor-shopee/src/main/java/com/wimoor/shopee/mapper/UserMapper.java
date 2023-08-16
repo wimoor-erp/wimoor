96b8524e37c2fd7f469b5bba4e93f9ad2216a788
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-08-16 12:15:57 +0800

    移除

--- wimoor-shopee/src/main/java/com/wimoor/shopee/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wimoor.shopee.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wimoor.shopee.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
