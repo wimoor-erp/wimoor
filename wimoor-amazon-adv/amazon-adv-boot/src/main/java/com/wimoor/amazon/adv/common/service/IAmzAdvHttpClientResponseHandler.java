154e875cbc3f5c288c4adbad2be5fa640042f242
深圳市万墨信息科技有限公司 <felix.liu.hn@gmail.com>
2023-08-16 12:14:48 +0800

    移除

--- wimoor-amazon-adv/amazon-adv-boot/src/main/java/com/wimoor/amazon/adv/common/service/IAmzAdvHttpClientResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.wimoor.amazon.adv.common.service;
-
-import com.alibaba.fastjson.JSONReader;
-import com.wimoor.amazon.adv.common.pojo.AmzAdvProfile;
-import com.wimoor.amazon.adv.common.pojo.AmzAdvRequest;
-
-public interface IAmzAdvHttpClientResponseHandler {
-	public Boolean treatReport(AmzAdvProfile profile, AmzAdvRequest request,JSONReader	 jsonReader) ;
-}
